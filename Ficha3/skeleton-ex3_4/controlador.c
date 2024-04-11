d700582ae8dc02a15f8851efa11035fe18c58da7
GonçaloCruz <a104346@alunos.uminho.pt>
2024-04-11 15:57:54 +0100

    Estudo

--- Ficha3/skeleton-ex3_4/controlador.c
+++ Ficha3/skeleton-ex3_4/controlador.c
@@ -40,29 +40,3 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
-
-
-
-
-// void controller(int N, char** commands){
-//     int fork_ret, status;
-//     int pids[N];
-//     for(int i = 0; i<N; i++){
-//         fork_ret = fork();
-//         if(fork_ret == 0){
-//             int counter = 0;
-//             int res = -1;
-//             while(res!=0){
-//                 res = mysystem(commands[i]);
-//                 counter++;
-//             }
-//             _exit(counter);
-//         }
-//         else{
-//             pids[i] = fork_ret;
-//         }
-//     }
-//     for(int i = 0; i<N; i++){
-//         waitpid(pids[i], &status, 0);
-//     }
-// }
